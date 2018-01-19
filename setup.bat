cc809fa4a8279ce04364846dc5bc30281b1b7c9e
ZippyMagician <joshuabarnett@ridgefieldps.net>
2018-01-19 10:29:48 -0500

    Installation automatic

--- setup.bat
+++ setup.bat
@@ -14,12 +14,10 @@ echo Installing dependencies...
 git clone https://github.com/ZippyMagician/CardBot.git
 echo Installed!
 echo ---
-echo ---
-echo Open a seperate terminal in this folder and use the following 3 commands
-echo ---
-echo npm install nodemon -g
-echo npm install discord.js
-echo npm install snekfetch --save
+call start C:/Users/Public/CardBot/Install/nodemon.bat
+call start C:/Users/Public/CardBot/Install/discord.js.bat
+call start C:/Users/Public/CardBot/Install/snekfetch.bat
+call start C:/Users/Public/CardBot/Install/mysql.bat
 echo Done!
 echo ---
 
