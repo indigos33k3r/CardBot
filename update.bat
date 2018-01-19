ac01d21323e9b52431c5eefb8c03f6227e9499f5
ZippyMagician <joshuabarnett@ridgefieldps.net>
2018-01-19 10:37:23 -0500

    Fix Update to script to include node modules

--- update.bat
+++ update.bat
@@ -3,6 +3,12 @@ cd C:/Users/Public/CardBot
 
 echo Updating...
 git clone https://github.com/ZippyMagician/CardBot.git
+
+call start C:/Users/Public/CardBot/Install/nodemon.bat
+call start C:/Users/Public/CardBot/Install/discord.js.bat
+call start C:/Users/Public/CardBot/Install/snekfetch.bat
+call start C:/Users/Public/CardBot/Install/mysql.bat
+
 echo Done!
 echo ---
 
