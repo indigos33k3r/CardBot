12c7b6e572c4150c3935c874bc55fb543ed3039a
ZippyMagician <joshuabarnett@ridgefieldps.net>
2018-01-19 10:36:17 -0500

    Finish install

---
+++ install/finish.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Hey!
+echo Just setting a few things up...
+echo ---
+cd C:/Users/Public/CardBot
+echo Done!
+echo ---
+echo Don't forget to install MySQL!
+echo To see a good tutorial on how to set it up, go to this link: https://www.youtube.com/watch?v=E5TVwyiCuYA&t=1s
+echo Watch it until he sets installs, sets up database/table, and goes over basic syntax!
+echo ---
+echo That is it
+echo Press enter to exit
+
+pause
