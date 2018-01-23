e9412d65c39e28f086150c73f9ff7e512ab88525
ZippyMagician <joshuabarnett@ridgefieldps.net>
2018-01-23 12:14:59 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -7,7 +7,7 @@ echo If you believe this is a mistake, close the batch file
 echo ---
 
 echo Updating...
-git clone https://github.com/ZippyMagician/CardBot.git
+git clone git://github.com/ZippyMagician/CardBot.git
 
 call start C:/Users/Public/CardBot/Install/nodemon.bat
 call start C:/Users/Public/CardBot/Install/discord.js.bat
