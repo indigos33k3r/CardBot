0b4038e5cbdc13510dd3b9f757c52af89807b382
ZippyMagician <joshuabarnett@ridgefieldps.net>
2018-01-23 12:12:32 -0500

    Test warning

--- update.bat
+++ update.bat
@@ -1,6 +1,11 @@
 @echo off
 cd C:/Users/Public/CardBot
 
+echo You are using the test branch for installation
+echo If you believe this is a mistake, close the batch file
+
+echo ---
+
 echo Updating...
 git clone https://github.com/ZippyMagician/CardBot.git
 
