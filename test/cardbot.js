8affa3a866502d894a2c013df728a544d503c218
ZippyMagician <joshuabarnett@ridgefieldps.net>
2018-01-24 12:05:02 -0500

    Cardbot test config

---
+++ test/cardbot.js
@@ -0,0 +1,18 @@
+class TestClass {
+  constructor() {
+    this.data = require('../lib/config/settings.json');
+  }
+  
+  testAll() {
+    for(var i in this.data) {
+      console.log(`${i}: ${this.data[i]}`);
+    }
+  }
+}
+const test = new TestClass();
+
+console.log("Test start:");
+
+test.testAll();
+
+console.log("Test successful");
