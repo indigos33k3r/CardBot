324f9256d16079a2713a12da7000880bc4fd9117
ZippyMagician <joshuabarnett@ridgefieldps.net>
2018-01-25 10:34:29 -0500

    Fix again

--- test/cardbot.sh
+++ test/cardbot.sh
@@ -1,5 +1,5 @@
 echo -e Fetching cURL data...
 
-curl https://card-bot.github.io/api/cards/all.json | python -c 'import json,sys; result=json.load(sys.stdin); print(result["'cards'"][0])'
+curl https://card-bot.github.io/api/cards/all.json | python -c 'import json,sys; result=json.load(sys.stdin); print("Fetched data[card name]: " + str(result["'cards'"][0]["'name'"]))'
 
 echo -e Done
