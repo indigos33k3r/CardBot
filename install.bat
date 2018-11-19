8ea6c92d5537e1113d199fe26c780418c97deb9e
ZippyMagician <zippymagician1@gmail.com>
2018-11-19 09:35:01 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,17 +1,11 @@
 @echo off
 echo Checking versions...
 
-echo Installing dependencies...
-git clone -b master --single-branch https://github.com/ZippyMagician/CardBot.git CardBot
-cd CardBot
-echo Installed!
-echo ---
 call start /install/nodemon.bat
 call start /install/discord.js.bat
 call start /install/snekfetch.bat
 call start /install/mysql.bat
 echo Done!
-echo ---
 
 echo Closing...
 call start /install/finish.bat
